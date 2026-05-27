c682f38e6d2d230a5be093abe589de3628fdd81a
Sara1502 <sara.moreira1520@gmail.com>
2026-05-27 17:01:52 -0300

    Js feito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,43 @@
 
-
 navigator.geolocation.getCurrentPosition(
     function (position) {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
 
-        console.log(lat, lon);
+        fetch(`https://api.openweathermap.org/data/4.0/onecall/current?${latitude}&lon=${longitude}&appid=54ac4573647eaab790fe699fb0ce1f22&lang=pt_br&units=metric`)
+            .then(res => res.json())
+            .then(data => {
+                const temp = Math.round(data.main.temp);
+                const description = data.weather[0].description;
+
+                const conteudo = document.getElementById('body')
+                conteudo.innerHTML = `     
+                    <main class="background ">
+                            <section class="temperature">
+                                <h1 class="graus">${temp}°C</h1>
+                                <h2 class="clima">${description}</h2>
+                            </section>
+                        </main>
+                `
+
+                const section = document.querySelector('section')
+                section.classList.remove('hot', 'cold', 'spring')
+                section.classList.add(addClassSection(temp))
+            }
+            )
     },
     function (error) {
         alert('This browser does not suport geolocation')
     }
 );
+
+
+function addClassSection() {
+    if (temp < 20) {
+        return ('cold')
+    } else if (temp >= 20 && temp <= 26) {
+        return ('spring')
+    } else {
+        return ('hot')
+    }
+}
