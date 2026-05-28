80e0ff48c103d033835d1fe2634c1b4a056b0819
Sara1502 <sara.moreira1520@gmail.com>
2026-05-28 13:31:30 -0300

    Ajustes no clima.js

--- api/clima.js
+++ api/clima.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+GPUShaderModule.exports = async function handler(req, res) {
     const { latitude, longitude } = req.query;
 
     if (!latitude || !longitude) {
